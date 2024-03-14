0bd61644ed82396838ebf6d5588b4a2b822f681f
nickicn <nicolelopes0008@gmail.com>
2024-03-14 10:58:12 -0300

    Finalizada a estilização do Result

--- silvinhaHealthApp/src/components/Form/ResultImc/index.js
+++ silvinhaHealthApp/src/components/Form/ResultImc/index.js
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function ResultImc() {
+export default function ResultImc(props) {
   return (
-    <View>
-      <Text>Mensagem</Text>
-      <Text>Resultado IMC</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        {props.messageResult}
+      </Text>
+      <Text style={styles.textIMC}>
+        {props.resultImc}
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -150,
+  },
+  text: {
+    fontSize: 18,
+    color: '#3DA74D',
+    fontWeight: 'bold'
+  },
+  textIMC: {
+    fontSize: 40,
+    color: '#3DA74D',
+    fontWeight: 'bold'
+  },
+})
